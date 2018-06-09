1da68af84cf6e83b561ce3dd4e221892d944ef5f
benoitgoupilleau <benoit.goupilleau@gmail.com>
2018-06-09 21:26:44 +0200

    Removed cluster to only use pm2

--- server/server.js
+++ server/server.js
@@ -1,34 +1,21 @@
-const cluster = require('cluster');
-const os = require('os');
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const router = require('./controllers/controllers');
-const mongoose = require('./db/mongoose.js');
+require('./db/mongoose.js');
 
 const port = process.env.PORT || 3000;
 
 
-if (cluster.isMaster  && process.env.NODE_ENV !== 'test' && process.env.ALLOW_CLUSTER) {
-  const cpuCount = os.cpus().length;
-  for (let i = 0; i < cpuCount; i++) {
-    cluster.fork();
-  }
-} else {
-  const app = express();
-  app.use(helmet());
-  app.use(cors());
-  app.use(bodyParser.json());
-  app.use(router);
+const app = express();
+app.use(helmet());
+app.use(cors());
+app.use(bodyParser.json());
+app.use(router);
 
-  app.listen(port, () => {
-    console.log(`Worker ${cluster.worker ? cluster.worker.id : "test"} running on port ${port}`);
-  });
-  module.exports = app;
-}
-
-cluster.on('exit', (worker) => {
-  console.log(`'Worker ${worker.id} died`);
-  cluster.fork();
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
+
+module.exports = app;
