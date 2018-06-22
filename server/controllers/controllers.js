81574eca844bd7c4f1d88f9b3238fe88ce6d4c25
benoitgoupilleau <benoit.goupilleau@gmail.com>
2018-06-22 19:45:12 +0200

    Added alert controller

--- server/controllers/controllers.js
+++ server/controllers/controllers.js
@@ -8,18 +8,20 @@ const bikes = require('./bikes');
 const sessionBikes = require('./sessionBikes');
 const sessionPlaces = require('./sessionPlaces');
 const sensors = require('./sensors');
+const alerts = require('./alerts');
 
 const router = express.Router();
 
 router.use(express.static('public'));
-router.use(users);
+router.use(alerts);
+router.use(bikes);
 router.use(entities);
 router.use(parkings);
-router.use(stations);
-router.use(bikes);
+router.use(sensors);
 router.use(sessionBikes);
 router.use(sessionPlaces);
-router.use(sensors);
+router.use(stations);
+router.use(users);
 
 router.get('/', async (req, res) => {
   res.status(200).send(`Hello from the API Bik'Box`)
